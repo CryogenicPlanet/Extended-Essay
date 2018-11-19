7f64c92ff54ef67a0e87a8fa520a3b3668e14bb6
Rahul Tarak <rahultarak12345@gmail.com>
2018-11-19 22:26:08 +0530

    Update entropy.py

--- EE/Hasher/entropy.py
+++ EE/Hasher/entropy.py
@@ -2,13 +2,14 @@ from zxcvbn import zxcvbn
 results = []
 fileInput = input("Enter File Path : ")
 unencrypted = open(fileInput,"r")
-while True:
-    line = unencrypted.readline()
-    if not line:
-        break
-   	result = zxcvbn(line)
-    results.append(result)
+words = []
+for elem in unencrypted:
+    elem = elem.split("\n")
+    words.append(elem[0])
 unencrypted.close()
+for elem in words:
+    result = zxcvbn(elem)
+    results.append(result)
 scores = results['score']
-import statistics 
+import statistics
 mean = statistics.mean(scores)
